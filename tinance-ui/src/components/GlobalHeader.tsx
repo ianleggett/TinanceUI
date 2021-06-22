d65fac58cf3b723ac0c79c4956fb936afa2a6a1f
米老朱 <laozhu.me@gmail.com>
2021-06-23 02:10:15 +0800

    make global header more responsive

--- tinance-ui/src/components/GlobalHeader.tsx
+++ tinance-ui/src/components/GlobalHeader.tsx
@@ -1,10 +1,32 @@
-import { AppBar, Button, Container, Toolbar, Typography } from '@material-ui/core';
+import {
+  AppBar,
+  Box,
+  Button,
+  Container,
+  Hidden,
+  IconButton,
+  List,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
+  SwipeableDrawer,
+  Toolbar,
+  Typography,
+} from '@material-ui/core';
 import { createStyles, makeStyles } from '@material-ui/core/styles';
-import { useCallback } from 'react';
-import { Link, useHistory } from 'react-router-dom';
+import AccountBalanceOutlinedIcon from '@material-ui/icons/AccountBalanceOutlined';
+import AttachMoneyOutlinedIcon from '@material-ui/icons/AttachMoneyOutlined';
+import ExpandMoreOutlinedIcon from '@material-ui/icons/ExpandMoreOutlined';
+import MenuOutlinedIcon from '@material-ui/icons/MenuOutlined';
+import MoreHorizOutlinedIcon from '@material-ui/icons/MoreHorizOutlined';
+import { useCallback, useState } from 'react';
+import { Link, useHistory, useLocation } from 'react-router-dom';
 
 const useStyles = makeStyles((theme) => {
   return createStyles({
+    root: {
+      flex: '0 0 64px',
+    },
     toolbar: {
       display: 'flex',
       justifyContent: 'space-between',
@@ -22,6 +44,21 @@ const useStyles = makeStyles((theme) => {
     },
     logo: {
       marginRight: theme.spacing(2),
+    },
+    menu: {
+      '& > * + *': {
+        margin: theme.spacing(1),
+      },
+    },
+    drawer: {
+      '& .MuiDrawer-paperAnchorLeft': {
+        width: '38vw',
+      },
+    },
+    buttons: {
+      '& > * + *': {
+        margin: theme.spacing(1),
+      },
     },
   });
 });
@@ -42,14 +79,39 @@ type PropsWithDefault = GlobalHeaderProps & DefaultProps;
 export const GlobalHeader: React.FC<GlobalHeaderProps> = (props) => {
   const classes = useStyles();
   const history = useHistory();
+  const { pathname } = useLocation();
+  const [showDrawer, setShowDrawer] = useState(false);
   const { title, logo, maxWidth } = props as PropsWithDefault;
+
+  const handleDrawerOpen = useCallback(() => {
+    setShowDrawer(true);
+  }, []);
+
+  const handleDrawerClose = useCallback(() => {
+    setShowDrawer(false);
+  }, []);
+
+  const handleGoToMarketsPage = useCallback(() => {
+    history.push('/markets');
+    setShowDrawer(false);
+  }, [history]);
+
+  const handleGoToOffersPage = useCallback(() => {
+    history.push('/offers');
+    setShowDrawer(false);
+  }, [history]);
+
+  const handleGoToTradesPage = useCallback(() => {
+    history.push('/trades');
+    setShowDrawer(false);
+  }, [history]);
 
   const handleGoToSigninPage = useCallback(() => {
     history.push('/signin');
   }, [history]);
 
   return (
-    <AppBar position="static" color="transparent" variant="outlined">
+    <AppBar position="static" color="transparent" variant="outlined" className={classes.root}>
       <Container maxWidth={maxWidth} disableGutters>
         <Toolbar className={classes.toolbar}>
           <Link to="/" className={classes.brand}>
@@ -58,9 +120,82 @@ export const GlobalHeader: React.FC<GlobalHeaderProps> = (props) => {
               {title}
             </Typography>
           </Link>
-          <Button color="primary" onClick={handleGoToSigninPage}>
-            Sign In
-          </Button>
+          <Hidden only={['sm', 'xs']}>
+            <Box className={classes.menu}>
+              <Button
+                variant="text"
+                color={pathname.startsWith('/markets') ? 'primary' : 'default'}
+                onClick={handleGoToMarketsPage}
+              >
+                Markets
+              </Button>
+              <Button
+                variant="text"
+                color={pathname.startsWith('/offers') ? 'primary' : 'default'}
+                onClick={handleGoToOffersPage}
+              >
+                Offers
+              </Button>
+              <Button
+                variant="text"
+                color={pathname.startsWith('/trades') ? 'primary' : 'default'}
+                onClick={handleGoToTradesPage}
+              >
+                Trades
+              </Button>
+              <Button variant="text" endIcon={<ExpandMoreOutlinedIcon />}>
+                More
+              </Button>
+            </Box>
+          </Hidden>
+
+          <Hidden mdUp>
+            <SwipeableDrawer
+              anchor="left"
+              open={showDrawer}
+              onClose={handleDrawerClose}
+              onOpen={handleDrawerOpen}
+              className={classes.drawer}
+            >
+              <List component="nav" aria-label="Main Menu">
+                <ListItem button divider>
+                  <ListItemIcon>
+                    <AccountBalanceOutlinedIcon />
+                  </ListItemIcon>
+                  <ListItemText primary="Markets" onClick={handleGoToMarketsPage} />
+                </ListItem>
+                <ListItem button divider>
+                  <ListItemIcon>
+                    <AttachMoneyOutlinedIcon />
+                  </ListItemIcon>
+                  <ListItemText primary="Offers" onClick={handleGoToOffersPage} />
+                </ListItem>
+                <ListItem button divider>
+                  <ListItemIcon>
+                    <AttachMoneyOutlinedIcon />
+                  </ListItemIcon>
+                  <ListItemText primary="Trades" onClick={handleGoToTradesPage} />
+                </ListItem>
+                <ListItem button divider>
+                  <ListItemIcon>
+                    <MoreHorizOutlinedIcon />
+                  </ListItemIcon>
+                  <ListItemText primary="More" />
+                </ListItem>
+              </List>
+            </SwipeableDrawer>
+          </Hidden>
+
+          <Box className={classes.buttons}>
+            <Button color="primary" variant="contained" onClick={handleGoToSigninPage}>
+              Sign In
+            </Button>
+            <Hidden mdUp>
+              <IconButton color="primary" aria-label="Open Menu" onClick={handleDrawerOpen}>
+                <MenuOutlinedIcon />
+              </IconButton>
+            </Hidden>
+          </Box>
         </Toolbar>
       </Container>
     </AppBar>
