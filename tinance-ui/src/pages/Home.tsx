178170d4ad8bd24154c6327916697f2ab6ff2bdd
ian <tech@techiaz.com>
2021-08-14 14:11:49 +0100

    Minor formatting changes

--- tinance-ui/src/pages/Home.tsx
+++ tinance-ui/src/pages/Home.tsx
@@ -1,4 +1,4 @@
-import { Typography } from '@material-ui/core';
+import { Card, CardContent, CardMedia, Typography } from '@material-ui/core';
 import Container from '@material-ui/core/Container';
 import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
@@ -17,7 +17,7 @@ const useStyles = makeStyles((theme) => ({
   video: {
     width: '95%',
     aspectRatio: '16 / 9',
-    borderRadius: 5,
+    borderRadius: 20,
   },
   content: {
     padding: theme.spacing(1),
@@ -48,6 +48,21 @@ const useStyles = makeStyles((theme) => ({
     'float': 'left',
     'margin-right': '10px',
   },
+  howTo: {
+    'padding': '50px',
+    'min-height': '200px',
+    'font-size': '16px',
+    'border-radius': '20px',
+    'margin': '10px',
+  },
+  icon2: {
+    'width': '70px',
+    'float': 'left',
+    'margin-right': '30px',
+  },
+  text2: {
+    display: 'block',
+  },
   main: {
     'margin-top': '30px',
   },
@@ -76,7 +91,7 @@ const HomePage: React.FC = () => {
           </Typography>
         </Grid>
         <Grid xs={12} sm={12} md={6} lg={4} xl={4} item>
-          <Paper elevation={5} className={classes.bubble}>
+          <Paper elevation={2} className={classes.bubble}>
             <img src="secure.jpg" alt="control" className={classes.icon} />
             <Typography component="p" variant="body1">
               Simple, automated trade flow - making it easier and more accessible to trade globally,
@@ -85,7 +100,7 @@ const HomePage: React.FC = () => {
           </Paper>
         </Grid>
         <Grid xs={12} sm={12} md={6} lg={4} xl={4} item>
-          <Paper elevation={5} className={classes.bubble}>
+          <Paper elevation={2} className={classes.bubble}>
             <img src="manage.jpg" alt="control" className={classes.icon} />
             <Typography component="p" variant="body1">
               Customer has total control – trades are authorized directly from your own wallet and
@@ -94,7 +109,7 @@ const HomePage: React.FC = () => {
           </Paper>
         </Grid>
         <Grid xs={12} sm={12} md={6} lg={4} xl={4} item>
-          <Paper elevation={5} className={classes.bubble}>
+          <Paper elevation={2} className={classes.bubble}>
             <img src="trust.jpg" alt="control" className={classes.icon} />
             <Typography component="p" variant="body1">
               No more &apos;trust issues&apos; - we use decentralized{' '}
@@ -104,58 +119,100 @@ const HomePage: React.FC = () => {
           </Paper>
         </Grid>
       </Grid>
-      <Paper elevation={5} className={classes.bigBubble}>
-        <Grid spacing={3} alignItems="center" direction={direction} container>
-          <Grid xs={6} sm={6} md={6} lg={6} xl={6} item>
-            {videoUrl.startsWith('https://www.youtube.com') ? (
-              <iframe
-                id="ytplayer"
-                title="Weclome to Tinance"
-                src={videoUrl}
-                frameBorder={0}
-                allowFullScreen
-                className={classes.video}
-              />
-            ) : (
-              <video src={videoUrl} preload="auto" loop controls className={classes.video} />
-            )}
+      <Grid
+        spacing={3}
+        alignItems="center"
+        direction={direction}
+        container
+        className={classes.main}
+      >
+        <Typography className={classes.bigBubble}>
+          <Grid spacing={3} alignItems="flex-start" direction={direction} container>
+            <Grid xs={6} sm={6} md={6} lg={6} xl={6} item>
+              {videoUrl.startsWith('https://www.youtube.com') ? (
+                <iframe
+                  id="ytplayer"
+                  title="Weclome to Tinance"
+                  src={videoUrl}
+                  frameBorder={0}
+                  allowFullScreen
+                  className={classes.video}
+                />
+              ) : (
+                <video src={videoUrl} preload="auto" loop controls className={classes.video} />
+              )}
+            </Grid>
+            <Grid xs={6} sm={6} md={6} lg={6} xl={6} className={classes.content} item>
+              <Typography component="h2" align="center" variant="h4" color="primary">
+                {t('How it works')}
+                <br />
+                <br />
+              </Typography>
+              <Card className={classes.howTo}>
+                <CardMedia
+                  component="img"
+                  className={classes.icon2}
+                  image="join.png"
+                  title="Join"
+                />
+
+                <CardContent>
+                  <Typography gutterBottom variant="h5" component="h2" color="primary">
+                    Create your account
+                  </Typography>
+                  <Typography variant="body2" className={classes.text2} component="p">
+                    <a href="signup">Sign up</a>, enter your contact details and your journey
+                    begins. It is that easy - and its free!
+                  </Typography>
+                </CardContent>
+              </Card>
+              <Card className={classes.howTo}>
+                <CardMedia
+                  component="img"
+                  className={classes.icon2}
+                  alt="wallet"
+                  image="wallet.png"
+                  title="connect your wallet"
+                />
+                <CardContent>
+                  <Typography gutterBottom variant="h5" component="h2" color="primary">
+                    Connect your eWallet
+                  </Typography>
+                  <Typography variant="body2" className={classes.text2} component="p">
+                    Link your eWallet using <a href="https://metamask.io/">Metamask</a> or
+                    compatible <a href="https://walletconnect.org/">Wallet Connect</a>. Add your
+                    receiving bank details and begin trading.
+                  </Typography>
+                </CardContent>
+              </Card>
+              <Card className={classes.howTo}>
+                <CardMedia
+                  component="img"
+                  className={classes.icon2}
+                  alt="complete trade"
+                  image="complete.png"
+                  title="complete your trade"
+                />
+                <CardContent>
+                  <Typography gutterBottom variant="h5" component="h2" color="primary">
+                    Complete your trade
+                  </Typography>
+                  <Typography variant="body2" className={classes.text2} component="p">
+                    Each trade is managed by a{' '}
+                    <a href="https://ethereum.org/en/developers/docs/smart-contracts/">
+                      smart contract
+                    </a>
+                    , crypto is held in secure escrow and only released when digitally verified by
+                    both parties. If either party fails to verifiy every part of the process, funds
+                    are automatcically refunded. Arbitration takes place in the unlikely event of
+                    disputes.
+                  </Typography>
+                </CardContent>
+              </Card>
+            </Grid>
           </Grid>
-          <Grid xs={6} sm={6} md={6} lg={6} xl={6} className={classes.content} item>
-            <Typography component="h2" variant="h4" color="primary">
-              {t('How it works')}
-              <br />
-            </Typography>
-            <Typography component="p" variant="body1">
-              <b>Create your account</b>
-            </Typography>
-            <Typography component="p" variant="body1" />
-            <a href="signup">Sign up</a>, enter your contact details and your journey begins. It is
-            that easy - and its free!
-            <Typography component="p" variant="body1">
-              <b>Connect your eWallet</b>
-            </Typography>
-            <Typography component="p" variant="body1" />
-            Link your eWallet using <a href="https://metamask.io/">Metamask</a> or compatible{' '}
-            <a href="https://walletconnect.org/">Wallet Connect</a>. Add your recieving bank details
-            and begin trading.
-            <Typography component="p" variant="body1">
-              <b>Browse the marketplace and trade with other users</b>
-            </Typography>
-            <Typography component="p" variant="body1" />
-            <b>Accept</b> trades from other users or <b>Create</b> your own offer. Simply set your
-            terms and publish to the market for others to accept.
-            <Typography component="p" variant="body1">
-              <b>Complete your trade</b>
-            </Typography>
-            <Typography component="p" variant="body1" />
-            Each trade is managed by a{' '}
-            <a href="https://ethereum.org/en/developers/docs/smart-contracts/">smart contract</a>,
-            crypto is held in secure escrow and only released when digitally verified by both
-            parties. If either party fails to verifiy every part of the process, funds are
-            automatcically refunded. Arbitration takes place in the unlikely event of disputes.
-          </Grid>
-        </Grid>
-      </Paper>
+        </Typography>
+      </Grid>
     </Container>
   );
 };
